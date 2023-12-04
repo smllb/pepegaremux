667f11554e04cd5f77874322a0b88100e15c0336
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-04 07:04:08 -0300

    Mid component. (Middle section of GridContainer)

---
+++ react-pepegaremux/src/components/grid-container/cells/mid/Mid.js
@@ -0,0 +1,10 @@
+import DownloadButton from '../../../GenericButton'
+import VideoList from '../../../VideoList/VideoList';
+function Mid() {
+    return (
+      <VideoList/>
+    );
+  }
+  
+  export default Mid;
+
