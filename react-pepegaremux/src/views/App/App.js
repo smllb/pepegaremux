2e92fe6cb47db2f4f7bad9a7959d4df864d1a5c7
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-04 07:06:37 -0300

    Updated App.js to display GridContainer.

--- react-pepegaremux/src/views/App/App.js
+++ react-pepegaremux/src/views/App/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import Top from '../../components/top/Top';
+import GridContainer from '../../components/grid-container/GridContainer.js';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Top/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">       
+        <GridContainer/>
       </header>
     </div>
   );
