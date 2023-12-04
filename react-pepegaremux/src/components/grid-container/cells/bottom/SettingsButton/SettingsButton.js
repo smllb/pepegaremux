4d02bd30245a91dfbb853278cf6e9d4b566bc6ad
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-04 06:58:15 -0300

    SettingsButton component (Open settings view)

---
+++ react-pepegaremux/src/components/grid-container/cells/bottom/SettingsButton/SettingsButton.js
@@ -0,0 +1,16 @@
+
+import { Button } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const SettingsButton = () => {
+     
+    return (
+
+        <Button variant="contained" startIcon={<SettingsIcon />}>
+        Settings
+        </Button>
+            
+    )    
+}
+
+export default SettingsButton
