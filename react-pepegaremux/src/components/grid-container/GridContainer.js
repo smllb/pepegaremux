69cb2d0bd5b5c7e82de64c3805f354f73eb80f2b
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-27 02:06:20 -0300

    feat: now providing socket and pageHeight contexts for components inside the grid

--- react-pepegaremux/src/components/grid-container/GridContainer.js
+++ react-pepegaremux/src/components/grid-container/GridContainer.js
@@ -1,32 +1,40 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import './GridContainer.css';
 import Top from './cells/top/Top';
 import Mid from './cells/mid/Mid';
 import Bottom from './cells/bottom/Bottom';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import io from 'socket.io-client';
+
+export const PageHeightContext = React.createContext()
+export const SocketContext = React.createContext();
+const socket = io('http://localhost:3967');
+
 function GridContainer() {
-
-  const matches = useMediaQuery('(max-height:550px)');
-
+  let pageHeightIsSmall = useMediaQuery('(max-height:550px)')
       
     return (
+      
         <Box sx={{ flexGrow: 1, width: '100vw', height: '100vh' }}>
+        <SocketContext.Provider value={socket}>
         <Grid container spacing={0}>
-          <Grid item xs={12} md={12} sx={{height: matches ? '20vh' : '15vh'}}>
-            <Top/>
+        <PageHeightContext.Provider value={pageHeightIsSmall}>
+          <Grid item xs={12} md={12} sx={{height: pageHeightIsSmall ? '20vh' : '15vh'}}>
+            <Top pageHeightIsSmall={pageHeightIsSmall}/>
           </Grid>
-          <Grid item xs={12} md={12} sx={{height: matches ? '60vh' : '72vh'}}>
+          <Grid item xs={12} md={12} sx={{height: pageHeightIsSmall ? '60vh' : '72vh'}}>
            <Mid/>
           </Grid>
-          <Grid item xs={12} md={12} sx={{height: matches ? '20vh' : '13vh'}}>
+          </PageHeightContext.Provider>
+          <Grid item xs={12} md={12} sx={{height: pageHeightIsSmall ? '20vh' : '13vh'}}>
             <Bottom/>
           </Grid>
         </Grid>
-      </Box>)
+        </SocketContext.Provider>
+      </Box>
+      )
   };
   
   export default GridContainer;
