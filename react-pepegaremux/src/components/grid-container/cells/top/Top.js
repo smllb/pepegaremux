188b8414068b9a97d1c3f63b45ff2cd24922178f
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-27 02:07:21 -0300

    refactor: minor formatting changes on the code

--- react-pepegaremux/src/components/grid-container/cells/top/Top.js
+++ react-pepegaremux/src/components/grid-container/cells/top/Top.js
@@ -1,11 +1,13 @@
 import InputDownloadForm from '../../../InputForm/InputForm';
+import DownloadButton from '../../../DownloadButton/DownloadButton';
 import pepeLogo from '../../../../static/images/pepega.png';
 
 function Top() {
     return (
-        <div style={{ display: 'flex', backgroundColor: "#1e2024", height: '100%', justifyContent: 'center', alignItems: 'center'} }>
+        <div style={{  display: 'flex', backgroundColor: "#1e2024", height: '100%', justifyContent: 'center', alignItems: 'center'} }>
             <img src={pepeLogo} style={{height: '40px'}}></img>
             <InputDownloadForm/>
+            <DownloadButton/> 
         </div>
     );
 }
