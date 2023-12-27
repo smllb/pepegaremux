b9a201fc7c9dfb5165e09d62b25cd444af76f861
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-27 02:04:19 -0300

    feat: added download button on top

---
+++ react-pepegaremux/src/components/DownloadButton/DownloadButton.js
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button';
+
+
+const downloadAllVideosFromList = () => {
+
+}
+
+function DownloadButton() {
+  return (
+    <Button variant="contained" color="primary" onClick={downloadAllVideosFromList}>
+      Download
+    </Button>
+  );
+}
+
+export default DownloadButton
