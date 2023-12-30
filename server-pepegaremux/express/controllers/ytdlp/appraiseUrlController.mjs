d24f2a695bf5ed97ded78e21e513eb8c6f4d278e
yogi <samuel.moreira6@fatec.sp.gov.br>
2023-12-30 01:16:41 -0300

    fix: added more use cases related to youtube link structure

--- server-pepegaremux/express/controllers/ytdlp/appraiseUrlController.mjs
+++ server-pepegaremux/express/controllers/ytdlp/appraiseUrlController.mjs
@@ -1,11 +1,13 @@
 const appraiseUrl = async (req, res) => {
-    console.log("url");
+    
     const url = decodeURIComponent(req)
+    console.log(`${url} being appraised.`);
     if (url.includes("www.youtube") || 
         url.includes("list=") || 
-        url.includes("/playlist?List=" ||
-        url.includes("watch?v=" ||
-        url.includes("/v/")))) {
+        url.includes("/playlist?List=") ||
+        url.includes("watch?v=") ||
+        url.includes("/v/") ||
+        url.includes("youtu.be")) {
         return 'YOUTUBE' 
     } 
     return 'GENERIC'
