b7698cabe57038e274898386a08bd44c21cccfdb
Samuel <samuelbarbosa.ipms@gmail.com>
2024-01-23 04:14:05 -0300

    chore: separating this function

---
+++ utils/writeFileTypeIntoSettings.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const writeFileTypeIntoSettings = (filetype) => {
+    let settingsPath = './settings.json'
+
+    if (fs.readFileSync(settingsPath)) {
+        console.log("Writing filetype into settings: " + filetype)
+        let settings = JSON.parse(fs.readFileSync(settingsPath))
+        settings.filetype = filetype;
+        let updatedSettings = JSON.stringify(settings, null, 2)
+        fs.writeFileSync(settingsPath, updatedSettings, 'utf8')
+
+    } else {
+
+        let defaultSettings = {
+            filetype: "mp3",
+            outputPath: path.join(os.homedir(), 'Music', 'PepegaRemux')
+        }
+        
+        fs.writeFileSync(settingsPath, JSON.stringify(defaultSettings, null, 2))
+
+    }
+
+}
+
+module.exports =  {
+    writeFileTypeIntoSettings
+}
